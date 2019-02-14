64bf0a61d54e85fb2099576cde28c613e73e0cb9
Joseph <joseph@Josephs-MacBook-Pro.local>
2019-02-13 20:59:59 -0600

    Initial Commit

---
+++ SimpleTabUI/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SimpleTabUI
+//
+//  Created by Joseph on 2/13/19.
+//  Copyright © 2019 Joseph. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

15122670904c8ce870925a4bb07f700c78d8e4c4
Joseph <joseph@Josephs-MacBook-Pro.local>
2019-02-16 23:48:22 -0600

    A basic sheet reading UI, no score or display function yet

---
+++ SimpleTabUI/StaffView.swift
@@ -0,0 +1,22 @@
+//
+//  StaffView.swift
+//  SimpleTabUI
+//
+//  Created by Joseph on 2/16/19.
+//  Copyright © 2019 Joseph. All rights reserved.
+//
+
+import UIKit
+
+class StaffView: UIImageView {
+       
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

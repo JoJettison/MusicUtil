13f76b379fea08ea82c27c09c98dcb0d0b363007
Emily Knowles <tigerv3@Emilys-MacBook-Pro.local>
2019-02-17 17:09:55 -0600

    Got audio working

--- SimpleTabUI/EarTrainViewController.swift
+++ SimpleTabUI/EarTrainViewController.swift
@@ -7,14 +7,77 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SecondViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    let questions = ["Answer = A", "Answer = B", "Answer = C", "Answer = D", "Answer = E", "Answer = F", "Answer = G"]
+    let buttonTitles = ["A", "B", "C", "D", "E", "F", "G"]
+    let answers = ["A", "B", "C", "D", "E", "F", "G"]
+    
+    //Variables
+    var currentQuestion = 0
+    var audioPlayer = AVAudioPlayer()
+    
+    //Label
+    @IBOutlet weak var lbl: UILabel!
+    
+    //Answer Button
+    @IBAction func Action(_ sender: Any)
+    {
+        if ((sender as AnyObject).tag == Int(currentQuestion)-1)
+        {
+            print(currentQuestion)
+            print("Right")
+        }
+        else
+        {
+            print(currentQuestion)
+            print("Wrong")
+        }
+        
+        if (currentQuestion != questions.count)
+        {
+            newQuestion()
+        }
+    }
+    
+    //Play Button
+    @IBAction func Play(_ sender: Any)
+    {
+        audioPlayer.play()
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool)
+    {
+        newQuestion()
+    }
+    
+    func newQuestion()
+    {
+        //lbl.text = questions[currentQuestion]
+        
+        currentQuestion += 1
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let sound = Bundle.main.path(forResource: "A-Tone-His_Self-1266414414", ofType: "mp3")
+        
+        do
+        {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        }
+        catch
+        {
+            print ("Error with sound system")
+        }
     }
-
-
+    
+    
 }
 

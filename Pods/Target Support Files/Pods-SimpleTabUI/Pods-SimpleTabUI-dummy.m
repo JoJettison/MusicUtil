81cc45fb714ba271f714ac6003b89f516dd727f7
Marcus Maldonado <marcusmaldonado@marcus-macbook-pro.local>
2019-02-25 16:09:15 -0600

    pod files included

---
+++ Pods/Target Support Files/Pods-SimpleTabUI/Pods-SimpleTabUI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SimpleTabUI : NSObject
+@end
+@implementation PodsDummy_Pods_SimpleTabUI
+@end
---
+++ Pods/Target Support Files/Pods-SimpleTabUITests/Pods-SimpleTabUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SimpleTabUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SimpleTabUITests
+@end

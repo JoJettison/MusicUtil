5a01676ecd64b19e45af716c6f02f5269a2fdeba
Emily Knowles <tigerv3@Emilys-MacBook-Pro.local>
2019-04-05 09:12:25 -0500

    Both color changes (based off correctness) - audio buggy

---
+++ SimpleTabUI/SettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.swift
+//  MusicUtil
+//
+//  Created by Emily Knowles on 4/2/19.
+//  Copyright © 2019 Joseph. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

4ebb7b16829b38130cd85e2043b4a6eca7086e5f
Emily Knowles <tigerv3@Emilys-MacBook-Pro.local>
2019-04-23 09:22:41 -0500

    Ear trainer grid view fixed
    - had to relink the action function

--- SimpleTabUI/EarTrainViewController.swift
+++ SimpleTabUI/EarTrainViewController.swift
@@ -11,7 +11,7 @@ import AVFoundation
 
 class SecondViewController: UIViewController {
     
-    var keyboardView = true
+    var keyboardView = false
     
     let pianoAnswerDict = [0:"C", 1:"C#", 2:"D", 3:"D#", 4:"E", 5:"F", 6:"F#", 7:"G", 8:"G#", 9:"A", 10:"A#", 11:"B"]
     let whiteKeys = [0, 2, 4, 5, 7, 9, 11]

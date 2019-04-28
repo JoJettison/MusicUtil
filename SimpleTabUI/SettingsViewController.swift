8c5d2532c73be1f1686399c76232afcb837d4ad6
Joseph <joseph@Josephs-MacBook-Pro.local>
2019-04-28 15:48:35 -0500

    Added the legal info for tuner and icons8

--- SimpleTabUI/SettingsViewController.swift
+++ SimpleTabUI/SettingsViewController.swift
@@ -16,19 +16,22 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var gameSwitch: UISwitch!
     @IBOutlet weak var lifeSlider: UISlider!
     @IBOutlet weak var sliderVal: UILabel!
+    @IBOutlet weak var keyInputLbl: UILabel!
+    @IBOutlet weak var gameModeLbl: UILabel!
+    @IBOutlet weak var lifeLbl: UILabel!
+    @IBOutlet weak var aboutLbl: UILabel!
+    @IBOutlet weak var legalBox: UITextView!
+
     
-    var keyboardView: Bool = false
     
     override func viewDidLoad() {
         let museTab = tabBarController as! MuseTabBarController
         if keySwitch.isOn{
             museTab.keyView = true
-            keyboardView = true
             print("Keyboardview on")
         }
         else{
             museTab.keyView = false
-            keyboardView = false
             print("Keyboardview off")
         }
         
@@ -40,6 +43,8 @@ class SettingsViewController: UIViewController {
             museTab.gameMode = false
         }
         museTab.lifeNum = Int(lifeSlider.value)
+        aboutLbl.isHidden = true
+        legalBox.isHidden = true
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
@@ -49,12 +54,10 @@ class SettingsViewController: UIViewController {
         let museTab = tabBarController as! MuseTabBarController
         if keySwitch.isOn{
             museTab.keyView = true
-            keyboardView = true
             print("Keyboardview on")
         }
         else{
             museTab.keyView = false
-            keyboardView = false
             print("Keyboardview off")
         }
         if gameSwitch.isOn{
@@ -70,20 +73,20 @@ class SettingsViewController: UIViewController {
         let museTab = tabBarController as! MuseTabBarController
         if keySwitch.isOn{
             museTab.keyView = true
-            keyboardView = true
             print("Keyboardview on")
         }
         else{
             museTab.keyView = false
-            keyboardView = false
             print("Keyboardview off")
         }
         if gameSwitch.isOn{
             museTab.gameMode = true
+            lifeSlider.isEnabled = true
             print("gameplay enabled")
         }
         else{
             museTab.gameMode = false
+            lifeSlider.isEnabled = false
         }
          museTab.lifeNum = Int(lifeSlider.value)
     }
@@ -92,20 +95,20 @@ class SettingsViewController: UIViewController {
         let museTab = tabBarController as! MuseTabBarController
         if keySwitch.isOn{
             museTab.keyView = true
-            keyboardView = true
             print("Keyboardview on")
         }
         else{
             museTab.keyView = false
-            keyboardView = false
             print("Keyboardview off")
         }
         if gameSwitch.isOn{
             museTab.gameMode = true
             print("gameplay enabled")
+            lifeSlider.isEnabled = true
         }
         else{
             museTab.gameMode = false
+            lifeSlider.isEnabled = false
         }
         museTab.lifeNum = Int(lifeSlider.value)
     }
@@ -115,6 +118,67 @@ class SettingsViewController: UIViewController {
         let currentVal = Int(sender.value)
         
         sliderVal.text = "\(currentVal)"
+    }
+    
+    @IBAction func modeSwitchedGame(_ sender: UISwitch) {
+        if(sender.isOn){
+            lifeSlider.isEnabled = true
+        }
+        else{
+            lifeSlider.isEnabled = false
+        }
+    }
+    
+    @IBAction func settingsSelection(_ sender: UISegmentedControl) {
+        
+        switch(sender.selectedSegmentIndex){
+        case 0: // Settings
+        //TODO Hide view objects for the legal and resources tab
+            
+        //BEGIN [SHOW] SETTINGS ELEMENTS
+            gameModeLbl.isHidden = false
+            gameSwitch.isHidden = false
+            keyInputLbl.isHidden = false
+            keySwitch.isHidden = false
+            lifeSlider.isHidden = false
+            lifeLbl.isHidden = false
+            sliderVal.isHidden = false
+        //END [SHOW] SETTINGS ELEMENTS
+            
+        //BEGIN [HIDE] RESOURCE ELEMENTS
+        //END [HIDE] RESOURCE ELEMENTS
+            
+        //BEGIN [HIDE] LEGAL ELEMENTS
+            legalBox.isHidden = true
+            aboutLbl.isHidden = true
+        //END [HIDE] LEGAL ELEMENTS
+            
+        case 1: // Resources
+            gameModeLbl.isHidden = true
+            gameSwitch.isHidden = true
+            keyInputLbl.isHidden = true
+            keySwitch.isHidden = true
+            lifeSlider.isHidden = true
+            lifeLbl.isHidden = true
+            sliderVal.isHidden = true
+            legalBox.isHidden = true
+            aboutLbl.isHidden = true
+            
+        case 2: // Legal
+            gameModeLbl.isHidden = true
+            gameSwitch.isHidden = true
+            keyInputLbl.isHidden = true
+            keySwitch.isHidden = true
+            lifeSlider.isHidden = true
+            lifeLbl.isHidden = true
+            sliderVal.isHidden = true
+            legalBox.isHidden = false
+            aboutLbl.isHidden = false
+            
+         
+        default:
+            gameModeLbl.isHidden = false
+        }
     }
     /*
     // MARK: - Navigation
